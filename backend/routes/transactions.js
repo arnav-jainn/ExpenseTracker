5955518b2b48b155f643ac750a0e20aa2a218981
Arnav jain <arnavjainn02@gmail.com>
2024-08-21 17:54:37 +0530

    frontend

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { addIncome } = require("../controllers/income");
+const { addIncome, getIncome, deleteIncome } = require("../controllers/income");
+const { addExpense, getExpense, deleteExpense } = require("../controllers/expense");
 const router = express.Router()
 
 
@@ -8,5 +9,10 @@ const router = express.Router()
 // })
 
 router.post('/add-income', addIncome)                //setting endpoint for posting and a method comming from controllers
+    .get('/get-incomes',getIncome)
+    .delete('/delete-income/:id',deleteIncome)
+    .post('/add-expense',addExpense)
+    .get('/get-expense',getExpense)
+    .delete('/delete-expense/:id',deleteExpense)
 
 module.exports = router
