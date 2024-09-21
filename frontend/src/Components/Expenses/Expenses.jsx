df0e6e3b76dab8bf148b2f25d161fe94aaa98116
Arnav jain <112501253+arnav-jainn@users.noreply.github.com>
2024-09-21 09:43:19 +0530

    Update Expenses.jsx

--- frontend/src/Components/Expenses/Expenses.jsx
+++ frontend/src/Components/Expenses/Expenses.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
-import { useGlobalContext } from "../../context/globalContext.jsX";
+import { useGlobalContext } from "../../context/globalContext.jsx";
 import IncomeItem from "../IncomeItem/IncomeItem";
 import ExpenseForm from "../ExpenseForm/ExpenseForm";
 
