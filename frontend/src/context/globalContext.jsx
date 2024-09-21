fd844bfdefb8523198b873a3036abd0b4965d5ae
Arnav jain <arnavjainn02@gmail.com>
2024-09-21 09:24:48 +0530

    deploy

--- frontend/src/context/globalContext.jsx
+++ frontend/src/context/globalContext.jsx
@@ -3,7 +3,7 @@
 import React, { useContext, useState } from "react";
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5001/api/v1";
+const BASE_URL = "https://expensetracker-ubt4.onrender.com";
 
 const GlobalContext = React.createContext();
 
