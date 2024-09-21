7e230b337f2a458b21873463507dea206b7167c2
Arnav jain <arnavjainn02@gmail.com>
2024-09-21 22:07:24 +0530

    deploy

--- frontend/src/context/globalContext.jsx
+++ frontend/src/context/globalContext.jsx
@@ -3,7 +3,7 @@
 import React, { useContext, useState } from "react";
 import axios from "axios";
 
-const BASE_URL = "https://expense-tracker-backend-mocha.vercel.app";
+const BASE_URL = "https://expense-tracker-backend-sage.vercel.app/";
 
 const GlobalContext = React.createContext();
 
